81817930f606710bf2fb050136113a51998fe0a9
jaimetrovoada <jaimetrovoada@gmail.com>
2023-08-29 18:36:50 +0800

    create header component

---
+++ components/header.tsx
@@ -0,0 +1,78 @@
+"use client";
+import localFont from "next/font/local";
+import { cn } from "@/lib/utils";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "./ui/navigation-menu";
+
+const phatt = localFont({
+  src: "../assets/fonts/Phatt.ttf",
+  variable: "--font-phatt",
+  display: "swap",
+});
+
+interface Props {
+  categories: Record<string, string>[];
+  brands: Record<string, string>[];
+}
+
+const Header = ({ categories, brands }: Props) => {
+  return (
+    <header
+      className={cn(
+        "relative border-b border-b-gray-200 p-2 flex flex-row justify-between",
+      )}
+    >
+      <h1 className={cn("text-3xl", phatt.className)}>OS</h1>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Menu</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid gap-3 p-4 w-screen lg:grid-cols-[1fr_1fr_1fr]">
+                <li className="row-span-3 text-lg font-semibold">
+                  <span className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md">
+                    All Products
+                  </span>
+                </li>
+                <li>
+                  <span className="text-lg font-semibold underline">
+                    Categories
+                  </span>
+                  <ul className="flex flex-row flex-wrap gap-4 p-2">
+                    {categories.map((category) => (
+                      <li
+                        key={category.slug}
+                        className="underline text-gray-700"
+                      >
+                        {category.title}
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+                <li>
+                  <span className="text-lg font-semibold underline">
+                    Brands
+                  </span>
+                  <ul className="flex flex-row flex-wrap gap-4 p-2">
+                    {brands.map((brand) => (
+                      <li key={brand.slug} className="underline text-gray-700">
+                        {brand.title}
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </header>
+  );
+};
+
+export default Header;
