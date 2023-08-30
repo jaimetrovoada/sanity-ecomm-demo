b5d2d5d50521f5bea68f8f1de45bcfcc469af90b
jaimetrovoada <jaimetrovoada@gmail.com>
2023-08-30 13:43:38 +0800

    add types

---
+++ @types/index.ts
@@ -0,0 +1,55 @@
+import { Image } from "sanity";
+
+export interface Product {
+  price: number;
+  _rev: string;
+  _id: string;
+  _updatedAt: string;
+  _type: "product";
+  title: string;
+  brand: {
+    title: string;
+    _type: "brand";
+  };
+  slug: {
+    current: string;
+    _type: "slug";
+  };
+  description: string;
+  images: Array<Image>;
+  tags: Array<{
+    title: string;
+    slug: {
+      current: string;
+      _type: "slug";
+    };
+    _type: "category";
+  }>;
+  _createdAt: string;
+}
+
+export interface Category {
+  _rev: string;
+  _type: "category";
+  _id: string;
+  title: string;
+  _updatedAt: string;
+  slug: {
+    current: string;
+    _type: "slug";
+  };
+  _createdAt: string;
+}
+
+export interface Brand {
+  title: string;
+  _updatedAt: string;
+  slug: {
+    current: string;
+    _type: "slug";
+  };
+  _createdAt: string;
+  _rev: string;
+  _type: "brand";
+  _id: string;
+}
