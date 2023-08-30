3385670fd40978708a8d0d80262eb1220681213c
jaimetrovoada <jaimetrovoada@gmail.com>
2023-08-30 17:59:56 +0800

    modify filtering logic

--- components/productList.tsx
+++ components/productList.tsx
@@ -8,20 +8,18 @@ interface Props {
 }
 
 const ProductList = ({ products, category, brand }: Props) => {
-  const filteredProducts = products?.filter(
-    (product) =>
-      product.brand.slug.current === brand ||
-      product.tags.some((tag) => tag.slug.current === category),
-  );
-
-  const finalProducts =
-    filteredProducts && filteredProducts.length > 0
-      ? filteredProducts
-      : products;
+  const productsFromSelectedBrand = products?.filter((product) => {
+    if (!brand) return true;
+    return product.brand.slug.current === brand;
+  });
+  const filteredProducts = productsFromSelectedBrand?.filter((product) => {
+    if (!category) return true;
+    return product.tags.some((tag) => tag.slug.current === category);
+  });
 
   return (
     <div className="grid grid-cols-2 gap-4 md:grid-cols-3 xl:grid-cols-4">
-      {finalProducts?.map((product) => (
+      {filteredProducts?.map((product) => (
         <ProductCard key={product.slug.current} product={product} />
       ))}
     </div>
