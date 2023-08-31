1d0fe4d0aa08a5f9012d62fcc8fc8186602aafcf
jaimetrovoada <jaimetrovoada@gmail.com>
2023-08-31 11:19:26 +0800

    improve product page responsiveness

--- app/(app)/store/products/page.tsx
+++ app/(app)/store/products/page.tsx
@@ -1,5 +1,6 @@
 import FilterList from "@/components/filterList";
 import ProductList from "@/components/productList";
+import SendOrderBtn from "@/components/sendOrderBtn";
 import { getBrands, getCategories, getProducts } from "@/lib/queries";
 import { cn } from "@/lib/utils";
 
@@ -8,19 +9,24 @@ interface Props {
 }
 const Page = async ({ searchParams }: Props) => {
   const { category, brand } = searchParams;
-  const [products, pErr] = await getProducts();
+  const [products, _] = await getProducts({
+    category: category as string,
+    brand: brand as string,
+  });
   const [categories, cErr] = await getCategories();
   const [brands, bErr] = await getBrands();
 
-  console.log({ category, brand });
+  console.log({ products, length: products?.length });
 
   return (
     <main>
-      <div className={cn("flex flex-row gap-4")}>
-        <aside className="w-1/6">
-          <h2 className={cn("font-semibold")}>Categories</h2>
-          <FilterList list={categories} parameter="category" />
-          <section>
+      <div className={cn("flex flex-col md:flex-row gap-4")}>
+        <aside className="flex md:flex-col md:w-1/6">
+          <section className="flex-1 p-2 md:flex-none">
+            <h2 className={cn("font-semibold")}>Categories</h2>
+            <FilterList list={categories} parameter="category" />
+          </section>
+          <section className="flex-1 p-2 md:flex-none">
             <h2 className={cn("font-semibold")}>Brands</h2>
             <FilterList list={brands} parameter="brand" />
           </section>
--- components/filterList.tsx
+++ components/filterList.tsx
@@ -33,7 +33,6 @@ const FilterList = ({ parameter, list }: Props) => {
     },
     [searchParams],
   );
-  console.log({ pathname, searchParams });
 
   const value = searchParams.get(parameter) || "all";
   const current = list?.find((item) => item.slug.current === value);
@@ -51,37 +50,35 @@ const FilterList = ({ parameter, list }: Props) => {
     : [];
 
   return (
-    <div className="p-2">
-      <Select
-        value={value}
-        onValueChange={(value) => {
-          return router.push(
-            pathname + "?" + createQueryString(parameter, value),
-          );
-        }}
-      >
-        <SelectTrigger className="capitalize">
-          <SelectValue
-            className="capitalize"
-            placeholder={parameter}
-            aria-label={current?.title}
+    <Select
+      value={value}
+      onValueChange={(value) => {
+        return router.push(
+          pathname + "?" + createQueryString(parameter, value),
+        );
+      }}
+    >
+      <SelectTrigger className="capitalize">
+        <SelectValue
+          className="capitalize"
+          placeholder={parameter}
+          aria-label={current?.title}
+        >
+          {current?.title || "All"}
+        </SelectValue>
+      </SelectTrigger>
+      <SelectContent>
+        {options?.map((item) => (
+          <SelectItem
+            key={item.slug.current}
+            value={item.slug.current}
+            className="flex"
           >
-            {current?.title || "All"}
-          </SelectValue>
-        </SelectTrigger>
-        <SelectContent>
-          {options?.map((item) => (
-            <SelectItem
-              key={item.slug.current}
-              value={item.slug.current}
-              className="flex"
-            >
-              {item.title}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
-    </div>
+            {item.title}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
   );
 };
 
