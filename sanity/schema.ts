ace47c0f743ce28129eed84a0352e6d0d8f84002
jaimetrovoada <jaimetrovoada@gmail.com>
2023-08-31 12:59:11 +0800

    create order schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -2,7 +2,8 @@ import { type SchemaTypeDefinition } from "sanity";
 import product from "./schema/product";
 import category from "./schema/category";
 import brand from "./schema/brand";
+import order from "./schema/order";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [product, category, brand],
+  types: [product, category, brand, order],
 };
---
+++ sanity/schema/order.ts
@@ -0,0 +1,63 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "order",
+  title: "Order",
+  type: "document",
+  fields: [
+    defineField({
+      name: "orderId",
+      title: "Order ID",
+      type: "string",
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: "customer",
+      title: "Customer",
+      type: "object",
+      fields: [
+        defineField({
+          name: "name",
+          title: "Name",
+          type: "string",
+        }),
+        defineField({
+          name: "email",
+          title: "Email",
+          type: "string",
+        }),
+      ],
+    }),
+    defineField({
+      name: "products",
+      title: "Products",
+      type: "array",
+      of: [{ type: "reference", to: [{ type: "product" }] }],
+    }),
+    defineField({
+      name: "date",
+      title: "Date",
+      type: "datetime",
+    }),
+    defineField({
+      name: "status",
+      title: "Status",
+      type: "string",
+      options: {
+        list: [
+          { title: "Pending", value: "pending" },
+          { title: "Completed", value: "completed" },
+          { title: "Cancelled", value: "cancelled" },
+        ],
+      },
+    }),
+  ],
+});
