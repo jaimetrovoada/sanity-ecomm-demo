4576e93eb58dd8b0357e88e450ada420143b3f03
jaimetrovoada <jaimetrovoada@gmail.com>
2023-09-01 18:04:10 +0800

    change padding

--- components/header.tsx
+++ components/header.tsx
@@ -28,7 +28,7 @@ const Header = ({ categories, brands }: Props) => {
   return (
     <header
       className={cn(
-        "relative border-b border-b-gray-200 p-2 flex flex-row justify-between bg-white",
+        "relative border-b border-b-gray-200 py-2 px-4 flex flex-row justify-between bg-white",
       )}
     >
       <h1 className={cn("text-3xl", phatt.className)}>
