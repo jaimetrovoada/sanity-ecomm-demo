42d093ebc77d618d455a148e5bdcbe37ac4a35bf
jaimetrovoada <jaimetrovoada@gmail.com>
2023-09-02 12:20:40 +0800

    implement saving cart to localStorage

--- components/cart.tsx
+++ components/cart.tsx
@@ -8,9 +8,12 @@ import { ShoppingCart } from "lucide-react";
 import { Button } from "./ui/button";
 import { useCart } from "@/lib/cartReducer";
 import Link from "next/link";
+import { useIsClient } from "usehooks-ts";
 
 const Cart = () => {
   const { state } = useCart();
+
+  const isClient = useIsClient();
 
   const cartQuantity = state.cartItems.reduce((acc, item) => {
     return acc + item.quantity;
@@ -20,10 +23,10 @@ const Cart = () => {
     <NavigationMenuItem>
       <NavigationMenuTrigger className="">
         <ShoppingCart size={14} />
-        <span className="text-sm">Cart ({cartQuantity})</span>
+        <span className="text-sm">Cart ({isClient ? cartQuantity : 0})</span>
       </NavigationMenuTrigger>
-      <NavigationMenuContent className="p-4 w-screen md:w-[33vw] flex flex-col">
-        <h2 className="font-semibold text-lg">Cart</h2>
+      <NavigationMenuContent className="flex w-screen flex-col p-4 md:w-[33vw]">
+        <h2 className="text-lg font-semibold">Cart</h2>
         <section className="flex-1 p-2 text-gray-700">
           {state.cartItems.map((item) => (
             <div key={item.id} className="flex flex-row justify-between">
@@ -32,7 +35,7 @@ const Cart = () => {
             </div>
           ))}
         </section>
-        <section className="flex flex-row justify-between items-center">
+        <section className="flex flex-row items-center justify-between">
           <Button size="sm" asChild>
             <Link href="/store/checkout">Checkout</Link>
           </Button>
--- components/cartProvider.tsx
+++ components/cartProvider.tsx
@@ -1,17 +1,23 @@
 "use client";
 
-import { CartContext, cartReducer } from "@/lib/cartReducer";
-import { useReducer } from "react";
+import { CartContext, CartState, cartReducer } from "@/lib/cartReducer";
+import { useEffect, useReducer } from "react";
+import { useLocalStorage } from "usehooks-ts";
 
 interface Props {
   children: React.ReactNode;
 }
 
 const CartProvider = ({ children }: Props) => {
-  const [state, dispatch] = useReducer(cartReducer, {
+  const [lsState, setLsState] = useLocalStorage("cart", {
     cartItems: [],
     totalPrice: 0,
-  });
+  } as CartState);
+
+  const [state, dispatch] = useReducer(cartReducer, lsState);
+  useEffect(() => {
+    setLsState(state);
+  }, [state]);
 
   return (
     <CartContext.Provider
