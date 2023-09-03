0c38bf048b242997cbd823d0904587d4df8cf2cf
jaimetrovoada <jaimetrovoada@gmail.com>
2023-09-03 17:31:07 +0800

    feat: add featured section to store page

--- app/(app)/store/page.tsx
+++ app/(app)/store/page.tsx
@@ -1,7 +1,29 @@
+import FeaturedSection from "@/components/featuredSection";
+import { getCollections } from "@/lib/queries";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const Page = () => {
-  return <div>store</div>;
-};
+export default async function Page() {
+  const collections = await getCollections();
+  console.log({ collections });
+  if (collections instanceof Error) {
+    return (
+      <main className="space-y-4 p-2 md:p-4">
+        <p>crickets</p>
+      </main>
+    );
+  }
 
-export default Page;
+  return (
+    <main className="space-y-4 p-2 md:p-4">
+      {collections.map((collection) => (
+        <FeaturedSection
+          key={collection._id}
+          name={collection.title}
+          description={collection.description}
+          products={collection.recommendations}
+        />
+      ))}
+    </main>
+  );
+}
