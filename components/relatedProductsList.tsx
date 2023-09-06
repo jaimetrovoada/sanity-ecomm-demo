1be62e600e72662ed332c9004a7a20cf23637a3e
jaimetrovoada <jaimetrovoada@gmail.com>
2023-09-06 18:00:52 +0800

    chore: remove suspense from related products

--- components/relatedProductsList.tsx
+++ components/relatedProductsList.tsx
@@ -2,7 +2,6 @@ import { Product } from "@/@types";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
 import { getSameBrandProducts } from "@/lib/queries";
-import { Suspense } from "react";
 import ProductCard, { ProductCardSkeleton } from "./productCard";
 
 interface Props {
@@ -28,34 +27,15 @@ const RelatedProductsList = async ({ product }: Props) => {
         <ArrowRight className="h-4 w-4" />
       </h3>
       <ul className="mx-auto grid max-w-screen-lg snap-x snap-mandatory auto-cols-max grid-flow-col gap-4 overflow-auto scroll-smooth md:auto-cols-[20%]">
-        <Suspense
-          fallback={
-            <>
-              <li>
-                <ProductCardSkeleton />
-              </li>
-              <li>
-                <ProductCardSkeleton />
-              </li>
-              <li>
-                <ProductCardSkeleton />
-              </li>
-              <li>
-                <ProductCardSkeleton />
-              </li>
-            </>
-          }
-        >
-          {related ? (
-            related.map((product) => (
-              <li key={product._id} className="snap-start">
-                <ProductCard product={product} />
-              </li>
-            ))
-          ) : (
-            <p>Couldnt find anything</p>
-          )}
-        </Suspense>
+        {related ? (
+          related.map((product) => (
+            <li key={product._id} className="snap-start">
+              <ProductCard product={product} />
+            </li>
+          ))
+        ) : (
+          <p>Couldnt find anything</p>
+        )}
       </ul>
     </section>
   );
