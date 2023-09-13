5374206e787e9b09f8f67bce53e41d2311f9ca87
jaimetrovoada <jaimetrovoada@gmail.com>
2023-09-13 19:16:01 +0800

    chore: add scrollable area to related products

--- components/relatedProductsList.tsx
+++ components/relatedProductsList.tsx
@@ -3,6 +3,7 @@ import { ArrowRight } from "lucide-react";
 import Link from "next/link";
 import ProductCard, { ProductCardSkeleton } from "./productCard";
 import { getSameBrandProducts } from "@/lib/queries";
+import ScrollableArea from "./scrollableArea";
 
 interface Props {
   slug: string;
@@ -21,48 +22,46 @@ const RelatedProductsList = async ({ slug }: Props) => {
   }
 
   return (
-    <section>
+    <section className="mx-auto w-full max-w-screen-lg">
       <h2 className="mb-4 inline-flex items-center gap-1 text-2xl font-semibold hover:underline">
         <Link href={`/store/products?brand=${related[0].brand.slug}`}>
           More From {related[0].brand.title}
         </Link>
         <ArrowRight className="h-4 w-4" />
       </h2>
-      <ul className="mx-auto grid max-w-screen-lg snap-x snap-mandatory auto-cols-max grid-flow-col gap-4 overflow-auto scroll-smooth md:auto-cols-[20%]">
-        {related.map((product) => (
-          <li key={product._id} className="snap-start">
-            <ProductCard product={product} />
-          </li>
+      <ScrollableArea className="gap-2 [&>div]:shrink-0 [&>div]:basis-1/5">
+        {[...related, ...related, ...related].map((product) => (
+          <ProductCard product={product} key={product._id} />
         ))}
-      </ul>
+      </ScrollableArea>
     </section>
   );
 };
 
- const Skeleton = () => {
+const Skeleton = () => {
   return (
-      <section>
-        <h3 className="mb-4 inline-flex items-center gap-1 text-2xl font-semibold hover:underline">
-          More
-          <ArrowRight className="h-4 w-4" />
-        </h3>
-        <ul className="mx-auto grid max-w-screen-lg snap-x snap-mandatory auto-cols-[45%] grid-flow-col gap-4 overflow-auto scroll-smooth md:auto-cols-[20%]">
-          <li>
-            <ProductCardSkeleton />
-          </li>
-          <li>
-            <ProductCardSkeleton />
-          </li>
-          <li>
-            <ProductCardSkeleton />
-          </li>
-          <li>
-            <ProductCardSkeleton />
-          </li>
-        </ul>
-      </section>
-  )
-}
-RelatedProductsList.Skeleton = Skeleton
+    <section>
+      <h3 className="mb-4 inline-flex items-center gap-1 text-2xl font-semibold hover:underline">
+        More
+        <ArrowRight className="h-4 w-4" />
+      </h3>
+      <ul className="mx-auto grid max-w-screen-lg snap-x snap-mandatory auto-cols-[45%] grid-flow-col gap-4 overflow-auto scroll-smooth md:auto-cols-[20%]">
+        <li>
+          <ProductCardSkeleton />
+        </li>
+        <li>
+          <ProductCardSkeleton />
+        </li>
+        <li>
+          <ProductCardSkeleton />
+        </li>
+        <li>
+          <ProductCardSkeleton />
+        </li>
+      </ul>
+    </section>
+  );
+};
+RelatedProductsList.Skeleton = Skeleton;
 
 export default RelatedProductsList;
