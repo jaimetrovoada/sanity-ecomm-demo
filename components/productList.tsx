52cd4af7a8ded42662d50aa4348282eea70a060e
jaimetrovoada <jaimetrovoada@gmail.com>
2023-09-13 16:37:17 +0800

    chore: modify products grid

--- components/productList.tsx
+++ components/productList.tsx
@@ -7,17 +7,21 @@ interface Props {
 
 const ProductList = ({ products }: Props) => {
   if (!products) {
-    <div>
-      <p>Nothing here yet...</p>
-      <p>Stay Tuned!!!</p>
-    </div>;
+    return (
+      <div>
+        <p>Nothing here yet...</p>
+        <p>Stay Tuned!!!</p>
+      </div>
+    );
   }
 
   return (
-    <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
-      {products?.map((product) => (
-        <ProductCard key={product.slug.current} product={product} />
-      ))}
+    <div className="flex-1">
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-4 xl:grid-cols-5">
+        {products.map((product) => (
+          <ProductCard key={product.slug.current} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
