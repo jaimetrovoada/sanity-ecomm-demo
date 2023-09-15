a1b0a438b1233015eb523744c0f66a5ff1a52822
jaimetrovoada <jaimetrovoada@gmail.com>
2023-09-15 15:04:31 +0800

    fix: products page loading ui

--- app/(app)/store/products/loading.tsx
+++ app/(app)/store/products/loading.tsx
@@ -1,31 +1,31 @@
-import { ProductCardSkeleton } from "@/components/productCard";
+import ProductCard from "@/components/productCard";
 import FilterList from "@/components/filterList";
 import { cn } from "@/lib/utils";
 
 const Loading = () => {
   return (
-    <main className="p-2 md:p-4">
-      <div className={cn("flex flex-col gap-4 md:flex-row")}>
-        <aside className="flex md:w-1/6 md:flex-col">
-          <section className="flex-1 p-2 md:flex-none">
-            <h2 className={cn("font-semibold")}>Categories</h2>
-            <FilterList list={null} parameter="category" />
-          </section>
-          <section className="flex-1 p-2 md:flex-none">
-            <h2 className={cn("font-semibold")}>Brands</h2>
-            <FilterList list={null} parameter="brand" />
-          </section>
-        </aside>
-        <section className={cn("flex flex-1 flex-col")}>
-          <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
-            <ProductCardSkeleton />
-            <ProductCardSkeleton />
-            <ProductCardSkeleton />
-            <ProductCardSkeleton />
-            <ProductCardSkeleton />
-          </div>
+    <main className="container flex flex-1 flex-col gap-2 py-4 lg:flex-row">
+      <aside className="flex lg:sticky lg:top-0 lg:w-1/6 lg:flex-col lg:self-start">
+        <section className="flex-1 p-2 lg:flex-none">
+          <h2 className={cn("font-semibold")}>Categories</h2>
+          <FilterList list={null} parameter="category" />
         </section>
-      </div>
+        <section className="flex-1 p-2 lg:flex-none">
+          <h2 className={cn("font-semibold")}>Brands</h2>
+          <FilterList list={null} parameter="brand" />
+        </section>
+      </aside>
+      <section className={cn("flex flex-1 flex-col")}>
+        <div className="flex-1">
+          <div className="grid grid-cols-1 gap-2 md:grid-cols-4 md:gap-4 xl:grid-cols-5">
+            <ProductCard.Skeleton useListStyle />
+            <ProductCard.Skeleton useListStyle />
+            <ProductCard.Skeleton useListStyle />
+            <ProductCard.Skeleton useListStyle />
+            <ProductCard.Skeleton useListStyle />
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
