ad650fb0bd8cf2c8640b33c9e12acc83e1f377fe
jaimetrovoada <jaimetrovoada@gmail.com>
2023-09-16 13:46:57 +0800

    fix: hydration warnings

--- app/(app)/store/checkout/page.tsx
+++ app/(app)/store/checkout/page.tsx
@@ -5,10 +5,12 @@ import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/use-toast";
 import { CartActions, CartProduct, useCart } from "@/lib/cartReducer";
 import { MinusIcon, PlusIcon, TrashIcon } from "lucide-react";
+import { useIsClient } from "usehooks-ts";
 
 const Page = () => {
   const { state, dispatch } = useCart();
   const { toast } = useToast();
+  const isClient = useIsClient();
 
   const increaseItemAmount = (item: CartProduct) => {
     dispatch({
@@ -48,49 +50,51 @@ const Page = () => {
       <aside className="p-4 md:flex-1">
         <h2 className="mb-2 text-2xl font-semibold">Your Items</h2>
         <div>
-          {state.cartItems.map((item) => (
-            <div
-              key={item.id}
-              className="group flex flex-row items-center justify-between border-b border-gray-200 py-2"
-            >
-              <div className="flex flex-col">
-                <span>{item.name}</span>
-                <span className="text-sm text-gray-700">
-                  {item.price.toLocaleString("en-US", {
-                    style: "currency",
-                    currency: "USD",
-                  })}
-                </span>
-              </div>
-              <div className="flex flex-row gap-2">
-                <div className="flex flex-row items-center rounded-lg border border-gray-200 [&>*]:flex-1">
-                  <Button
-                    variant="ghost"
-                    size="icon"
-                    onClick={() => decreaseItemAmount(item)}
-                  >
-                    <MinusIcon size={16} />
-                  </Button>
-                  <span className="text-center">{item.quantity}</span>
-                  <Button
-                    variant="ghost"
-                    size="icon"
-                    onClick={() => increaseItemAmount(item)}
-                  >
-                    <PlusIcon size={16} />
-                  </Button>
-                </div>
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  className="group-hover:text-red-400"
-                  onClick={() => removeItem(item)}
+          {isClient
+            ? state.cartItems.map((item) => (
+                <div
+                  key={item.id}
+                  className="group flex flex-row items-center justify-between border-b border-gray-200 py-2"
                 >
-                  <TrashIcon size={16} />
-                </Button>
-              </div>
-            </div>
-          ))}
+                  <div className="flex flex-col">
+                    <span>{item.name}</span>
+                    <span className="text-sm text-gray-700">
+                      {item.price.toLocaleString("en-US", {
+                        style: "currency",
+                        currency: "USD",
+                      })}
+                    </span>
+                  </div>
+                  <div className="flex flex-row gap-2">
+                    <div className="flex flex-row items-center rounded-lg border border-gray-200 [&>*]:flex-1">
+                      <Button
+                        variant="ghost"
+                        size="icon"
+                        onClick={() => decreaseItemAmount(item)}
+                      >
+                        <MinusIcon size={16} />
+                      </Button>
+                      <span className="text-center">{item.quantity}</span>
+                      <Button
+                        variant="ghost"
+                        size="icon"
+                        onClick={() => increaseItemAmount(item)}
+                      >
+                        <PlusIcon size={16} />
+                      </Button>
+                    </div>
+                    <Button
+                      variant="ghost"
+                      size="icon"
+                      className="group-hover:text-red-400"
+                      onClick={() => removeItem(item)}
+                    >
+                      <TrashIcon size={16} />
+                    </Button>
+                  </div>
+                </div>
+              ))
+            : null}
         </div>
       </aside>
       <section className="flex flex-col rounded-md bg-white p-4 shadow-md md:flex-1">
