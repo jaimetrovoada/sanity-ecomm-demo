150549748d5673584d982396a617148923f78a83
jaimetrovoada <jaimetrovoada@gmail.com>
2023-11-16 10:22:39 +0000

    chore: handle error and no items states

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -14,10 +14,25 @@ const phatt = localFont({
 export default async function Home() {
   const collections = await getCollections();
 
-  if (collections instanceof Error) {
+  if (collections instanceof Error || !collections.length) {
     return (
-      <Main className="flex flex-col gap-4">
-        <p>crickets</p>
+      <Main className="flex flex-col gap-4 px-2 md:px-4">
+        <section className="relative flex h-[calc(100vh-2rem)] flex-col items-center justify-center overflow-hidden bg-[url('/images/hero_image.jpg')] bg-cover bg-center text-white">
+          <h2 className={`text-center text-5xl font-bold ${phatt.className}`}>
+            Oxygen Store
+          </h2>
+
+          <p className="mx-auto my-8 max-w-prose text-center text-xl">
+            Breathe new life into your wardrobe.
+          </p>
+          <Button
+            className="rounded-full border-2 border-white bg-gray-100/10 px-8 py-4 text-xl font-bold text-white backdrop-blur transition-all hover:bg-gray-100/25 hover:shadow-sm hover:shadow-white"
+            variant="link"
+            asChild
+          >
+            <Link href="/store">Shop Now</Link>
+          </Button>
+        </section>
       </Main>
     );
   }
